124f8eeaec6318150d9150fdbd580618ed9cb884
parkash-arjan <parkash.arjan.github@gmail.com>
2017-10-29 22:16:13 -0400

    updated printAll call with lambda function

--- JavaLambda/src/com/java8/lambda/FilterLambda3.java
+++ JavaLambda/src/com/java8/lambda/FilterLambda3.java
@@ -10,17 +10,19 @@ import com.java8.common.Person;
 public class FilterLambda3 {
 	public static void main(String[] args) {
 		List<Person> list = Arrays.asList(new Person("Parkash", 36), new Person("Divya", 36), new Person("Anira", 4), new Person("Samarth", 2), new Person("Samantha", 2));
-		printAll(list);
+
+		printByCriteria(list, person -> true);
+		// printAll(list); updated with lambda version
 
 		// Sort by name
 		Collections.sort(list, (person1, person2) -> person1.getName().toLowerCase().compareTo(person2.getName().toLowerCase()));
 
-		printAll(list);
+		printByCriteria(list, person -> true);
 
 		// Sort by age
 		Collections.sort(list, (person1, person2) -> Integer.valueOf(person1.getAge()).compareTo(Integer.valueOf(person2.getAge())));
 
-		printAll(list);
+		printByCriteria(list, person -> true);
 
 		// print person with age less than 10
 		printByCriteria(list, (person) -> person.getAge() < 10 ? true : false);
@@ -30,12 +32,12 @@ public class FilterLambda3 {
 
 	}
 
-	public static void printAll(List<Person> list) {
-		System.out.println("Person List");
-		for (Person person : list) {
-			System.out.println(person);
-		}
-	}
+	// public static void printAll(List<Person> list) {
+	// System.out.println("Person List");
+	// for (Person person : list) {
+	// System.out.println(person);
+	// }
+	// }
 
 	public static void printByCriteria(List<Person> list, IFilterCriteria filterCriteria) {
 		System.out.println("Person List With Criteria");
@@ -47,4 +49,3 @@ public class FilterLambda3 {
 
 	}
 }
-
