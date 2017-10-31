5d70e43173f7d79ba2d588a19014fc54d9dea261
parkash-arjan <parkash.arjan.github@gmail.com>
2017-10-31 11:32:33 -0400

    code formatted.

--- JavaLambda/src/com/java8/lambda/functions/FilterLambdaPredicateV1.java
+++ JavaLambda/src/com/java8/lambda/functions/FilterLambdaPredicateV1.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 
-import com.java8.common.IFilterCriteria;
 import com.java8.common.Person;
 
 public class FilterLambdaPredicateV1 {
