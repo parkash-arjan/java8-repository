73b102bc720f2241ed31fd8c7abd24554a9e9345
parkash-arjan <parkash.arjan.github@gmail.com>
2017-11-01 16:45:29 -0400

    code formatted

--- JavaLambda/src/com/java8/lambda/ListAggregationV2.java
+++ JavaLambda/src/com/java8/lambda/ListAggregationV2.java
@@ -6,19 +6,20 @@ import java.util.List;
 public class ListAggregationV2 {
 
 	public static void main(String[] args) {
-		List<Integer> ints = Arrays.asList(1, 3, 5, 2, 4, 6, 7);
+		List<Integer> ints = Arrays.asList(1, 3, 5, 2, 4, 6, 7, 10);
 
-		// System.out.println(aggregateV2(ints, (a, b) -> a + b));
-		// System.out.println(aggregateV2(ints, (a, b) -> Math.min(a, b)));
-		// System.out.println(aggregateV2(ints, (a, b) -> Math.max(a, b)));
-		System.out.println(aggregateV2(ints, (a, b) -> (a | a) + 1)); // count { (a, b) -> (a|a) + 1) , (a, b) -> (a) + 1) }
+		// System.out.println(perfom(ints, (a, b) -> a + b));
+		// System.out.println(perfom(ints, (a, b) -> Math.min(a, b)));
+		// System.out.println(perfom(ints, (a, b) -> Math.max(a, b)));
+		System.out.println(perfom(ints, (a, b) -> (a | a) + 1)); // count { (a,b) -> (a|a) + 1) , (a, b) -> (a) + 1) }
 
 	}
 
 	// lambda => (a,b)->a+b;
-	public static Integer aggregateV2(List<Integer> list, AggregateOperator operation) {
-		Integer result = 0;
-		for (Integer integer : list) {
+	public static Integer perfom(List<Integer> list, AggregateOperator operation) {
+		Integer result = list.get(0);
+		for (int i = 1; i < list.size(); i++) {
+			Integer integer = list.get(i);
 			result = operation.operate(result, integer);
 		}
 		return result;
